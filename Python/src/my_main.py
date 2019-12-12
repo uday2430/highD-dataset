770c9c51662523b8ca0e2fa6ab9b052ff625ca54
Maytheewat Aramrattana <maytheewat.aramrattana@vti.se>
2019-12-12 17:06:08 +0100

    added script to extract car-following period between different vehicles

--- Python/src/my_main.py
+++ Python/src/my_main.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import numpy as np
-import argparse
+import pandas as pd
 
 from data_management.read_csv_from_path import *
-
+from track_data_processing.find_car_following import *
 #frame rate is 25Hz
 
 range = np.arange(60)
@@ -13,7 +13,10 @@ for number in range:
     data_id.append((str((number+1)) if (number+1) >= 10 else "0"+str(number+1)))
 
 data_dir = "../data/"
-parser = argparse.ArgumentParser(description="ParameterOptimizer")
+results_dir = "../results/"
+
+EGO_TYPE = "Car"
+PRECEDING_TYPE = "Car"
 
 for id in data_id:
     print(id)
@@ -23,12 +26,16 @@ for id in data_id:
 
     recording_meta_data = read_meta_info(str(recording_meta_path))
     track_meta_data = read_static_info(str(track_meta_path))
-    #track_data = read_track_csv(str(track_path))
+    track_data = read_track_csv(str(track_path))
 
-    numCar = recording_meta_data.get('numCars')
-    numTruck = recording_meta_data.get('numTrucks')
-    percentageTrucks = (numTruck/(recording_meta_data.get('numVehicles')))*100
-    print("Number of cars: "+str(numCar))
-    print("Number of trucks: "+str(numTruck))
-    print("Percentage of trucks: " + str(percentageTrucks))
+    #to extract car-following period and save it to a file
+    car_following_data = find_car_following(track_meta_data, track_data, EGO_TYPE, PRECEDING_TYPE)
+    pd.DataFrame(car_following_data).to_csv(results_dir+id+"_"+EGO_TYPE+"_follow_"+PRECEDING_TYPE+".csv", index=False)
+
+    #numCar = recording_meta_data.get('numCars')
+    #numTruck = recording_meta_data.get('numTrucks')
+    #percentageTrucks = (numTruck/(recording_meta_data.get('numVehicles')))*100
+    #print("Number of cars: "+str(numCar))
+    #print("Number of trucks: "+str(numTruck))
+    #print("Percentage of trucks: " + str(percentageTrucks))
     print('-------------------------------')
---
+++ Python/src/track_data_processing/find_car_following.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def find_car_following(meta_data, data, my_type, preceding_type):
+    """
+    Find out car-following situations from specific vehicle types
+
+    :param meta_data: track meta information from highD dataset
+    :param data: track data from highD dataset
+    :param my_type: the ego vehicle's type
+    :param preceding_type: the preceiding vehicle's type
+
+    :return:
+    """
+    following_data = []
+    for id in range(0,len(data)):
+        following_duration = np.count_nonzero(data[id].get('precedingId'))
+        if following_duration != 0:
+            following_start = np.where(data[id].get('precedingId') > 0)[0][0]
+            if(
+                (get_vehicle_class(meta_data, data[id].get('precedingId')[following_start]) == preceding_type)
+                and (get_vehicle_class(meta_data, id+1) == my_type)
+            ):
+                #record the behaviour
+                following_data.append({"id": id, "following_start": following_start, "following_duration": following_duration})
+    return following_data
+
+def get_vehicle_class(meta_data, id):
+    """
+
+    :param meta_data: track meta information from highD dataset
+    :param id: vehicle's id in the dataset
+    :return: vehicle type (string: 'Car' or 'Truck')
+    """
+    return  meta_data[id].get('class')
