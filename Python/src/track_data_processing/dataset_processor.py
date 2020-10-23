88f6100be14ce31b259de04d261432d11f7d4470
Maytheewat Aramrattana <maytheewat.aramrattana@vti.se>
2020-10-23 17:56:54 +0200

    Add extraction of lane change trajectory

--- Python/src/track_data_processing/dataset_processor.py
+++ Python/src/track_data_processing/dataset_processor.py
@@ -1,5 +1,7 @@
 import statistics
 import numpy as np
+import matplotlib.pyplot as plt
+from .extraction_conditions import *
 
 def find_initial_state(meta_data, data):
     init_states = []
@@ -46,6 +48,7 @@ def get_initial_speed(data, id):
         if data[i].get('id') == id:
             speed = data[i].get('xVelocity')[0]
             return speed
+
 
 def find_car_following(meta_data, data, my_type, preceding_type):
     """
@@ -101,7 +104,7 @@ def find_car_following(meta_data, data, my_type, preceding_type):
                 preceding_id = data[i].get('precedingId')[frame]
                 if ((get_vehicle_class(meta_data, preceding_id) == preceding_type)
                         and (get_vehicle_class(meta_data, ego_id) == my_type)
-                        and (0 < data[i].get('thw')[frame] < 6)):
+                        and (CF_TGAP_LOWER_BOUND < data[i].get('thw')[frame] < CF_TGAP_UPPER_BOUND)):
                     if following_started is False:
                         following_started = True
                         frame_following_start = frame
@@ -169,6 +172,7 @@ def get_ttc(dhw, my_speed, pred_speed):
     else:
         return np.nan
 
+
 def find_lane_changes(meta_data, data):
     """
     Find out lane-changing situations from the dataset
@@ -183,18 +187,18 @@ def find_lane_changes(meta_data, data):
         totalCar: total number of cars
         totalTruck: total number of trucks
     """
-    n_car_lc = 0
     n_car = 0
+    n_car_lc = 0
     n_truck = 0
     n_truck_lc = 0
     for i in range(0, len(data)):
         ego_id = data[i].get('id')
         n_lc = meta_data[ego_id].get('numLaneChanges')
-        type = meta_data[ego_id].get('class')
-        if type == "Car":
+        vtype = meta_data[ego_id].get('class')
+        if vtype == "Car":
             n_car_lc = n_car_lc + n_lc
             n_car = n_car + 1
-        elif type == "Truck":
+        elif vtype == "Truck":
             n_truck_lc = n_truck_lc + n_lc
             n_truck = n_truck + 1
         else:
@@ -208,3 +212,81 @@ def find_lane_changes(meta_data, data):
         "totalTruck": n_truck
     }
     return lc_data
+
+
+def get_lane_change_trajectory(meta_data, data):
+    y_accel_car_LC = []
+    y_accel_truck_LC = []
+    y_accel_car_noLC = []
+    y_accel_truck_noLC = []
+    y_pos_car_LC = []
+    y_pos_car_noLC = []
+    y_pos_truck_LC = []
+    y_pos_truck_noLC = []
+    y_speed_car_LC = []
+    y_speed_car_noLC = []
+    y_speed_truck_LC = []
+    y_speed_truck_noLC = []
+    for i in range(0, len(data)):
+        ego_id = data[i].get('id')
+        n_lc = meta_data[ego_id].get('numLaneChanges')
+        vtype = meta_data[ego_id].get('class')
+        if n_lc > 0:
+            # this vehicle changes lane
+            # find lane change index
+            LC_index = []
+            totalFrames = len(data[i].get('laneId'))
+            lane_ids = list(data[i].get('laneId'))
+            current_lane = lane_ids[0]
+            for j in range(0, totalFrames):
+                if lane_ids[j] != current_lane:
+                    LC_index.append(j)
+                    current_lane = lane_ids[j]
+            for index in LC_index:
+                if LC_MARGIN_FRAMES < index < (totalFrames-LC_MARGIN_FRAMES):
+                    lower_bound = index-LC_MARGIN_FRAMES
+                    upper_bound = index+LC_MARGIN_FRAMES
+                    if vtype == "Car":
+                        y_accel_car_LC += list(data[i].get('yAcceleration')[lower_bound:upper_bound])
+                        y_speed_car_LC += list(data[i].get('yVelocity')[lower_bound:upper_bound])
+                        y_pos_car_LC = list(data[i].get('y')[lower_bound:upper_bound])
+                        #plt.plot(data[i].get('y'))
+                        #plt.axvline(x=index)
+                        #plt.show()
+                    elif vtype == "Truck":
+                        y_accel_truck_LC += list(data[i].get('yAcceleration')[lower_bound:upper_bound])
+                        y_speed_truck_LC += list(data[i].get('yVelocity')[lower_bound:upper_bound])
+                        y_pos_truck_LC = list(data[i].get('y')[lower_bound:upper_bound])
+                    else:
+                        print("vehicle class unknown")
+        else:
+            #this vehicle did not change lane
+            #find out average yAcceleration
+            # this vehicle changes lane
+            if vtype == "Car":
+                y_accel_car_noLC += list(data[i].get('yAcceleration'))
+                y_speed_car_noLC += list(data[i].get('yVelocity'))
+                y_pos_car_noLC = list(data[i].get('y'))
+            elif vtype == "Truck":
+                # if absolute value is desired >> [abs(a) for a in list(data[i].get('yAcceleration'))]
+                y_accel_truck_noLC += list(data[i].get('yAcceleration'))
+                y_speed_truck_noLC += list(data[i].get('yVelocity'))
+                y_pos_truck_noLC = list(data[i].get('y'))
+            else:
+                print("vehicle class unknown")
+    lc_trajectory = {
+        "Car_yAccel_LC": y_accel_car_LC,
+        "Car_yAccel_noLC": y_accel_car_noLC,
+        "Truck_yAccel_LC": y_accel_truck_LC,
+        "Truck_yAccel_noLC": y_accel_truck_noLC,
+        "Car_ySpeed_LC": y_speed_car_LC,
+        "Car_ySpeed_noLC": y_speed_car_noLC,
+        "Truck_ySpeed_LC": y_speed_truck_LC,
+        "Truck_ySpeed_noLC": y_speed_truck_noLC,
+        "Car_yPos_LC": y_pos_car_LC,
+        "Car_yPos_noLC": y_pos_car_noLC,
+        "Truck_yPos_LC": y_pos_truck_LC,
+        "Truck_yPos_noLC": y_pos_truck_noLC
+    }
+    return lc_trajectory
+
